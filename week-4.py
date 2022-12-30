4c2cf3c4838a0e7282b609ab106d5a36c641fc4a
Nasrul <98256020+naaasruul@users.noreply.github.com>
2022-12-30 11:43:34 +0800

    Add files via upload

---
+++ week-4.py
@@ -0,0 +1,34 @@
+name = "nasrul"
+print(name)
+
+age = "18"
+print(age)
+
+opinion = "this is my opinion.."
+print(opinion)
+
+# type of 80
+print(type(80))
+
+# type of 60.5
+print(type(60.5))
+
+print("enter your first name: ")
+userfirstName = input()
+print(userfirstName)
+
+
+print("enter your last name: ")
+userlastName = input()
+print(userlastName)
+
+print("enter your age: ")
+userAge = input()
+print(userAge)
+
+currentYear = 2022
+print("enter your year of born: ")
+bornYear = input()
+
+currentAge = currentYear - bornYear
+print(currentAge)
