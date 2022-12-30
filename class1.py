22164ff23ab6dc4378743a69586603be24780b4b
Nasrul <98256020+naaasruul@users.noreply.github.com>
2022-12-30 11:48:10 +0800

    Update and rename week-4.py to class1.py

--- class1.py
+++ class1.py
@@ -1,9 +1,12 @@
+# print your name
 name = "nasrul"
 print(name)
 
+# print your age
 age = "18"
 print(age)
 
+# print your opinion
 opinion = "this is my opinion.."
 print(opinion)
 
@@ -13,19 +16,22 @@ print(type(80))
 # type of 60.5
 print(type(60.5))
 
+# print user first name when they give an input
 print("enter your first name: ")
 userfirstName = input()
 print(userfirstName)
 
-
+# print user last name when they give an input
 print("enter your last name: ")
 userlastName = input()
 print(userlastName)
 
+# print user Age when they give an input
 print("enter your age: ")
 userAge = input()
 print(userAge)
 
+# calculate and print user age
 currentYear = 2022
 print("enter your year of born: ")
 bornYear = input()
