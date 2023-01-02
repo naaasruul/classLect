dcfc2af324a89bcff2c3de5344b4e435183281a2
Nasrul <98256020+naaasruul@users.noreply.github.com>
2023-01-02 23:57:11 +0800

    Add files via upload

---
+++ fizzBuzz.py
@@ -0,0 +1,17 @@
+# expected output:
+# fizz
+# buzz
+# fizz
+# buzz
+# fizzbuzz
+
+for x in range(1,100):
+
+    if x % 3 == 0 and x % 5 == 0:
+        print("fizzBuzz")
+    elif x % 3 == 0:
+        print("fizz")
+    elif x % 5 == 0:
+        print("buzz")
+    else:
+        print(x)    
